48ffdbff358aef5591bdef5c9b1b1fa1cb561189
Vitalii Kostiukov <vitalii.kostiukov@mev.com>
2025-09-09 11:10:06 +0300

    Add IAM user and policy for CD account, including access key outputs

---
+++ infra/setup/iam.tf
@@ -0,0 +1,49 @@
+####
+# Create IAM user for CD account #
+####
+
+resource "aws_iam_user" "cd" {
+  name = "udemy-terraform-cd-user"
+}
+
+resource "aws_iam_access_key" "cd" {
+  user = aws_iam_user.cd.name
+}
+
+####
+# Policy for CD user #
+####
+
+data "aws_iam_policy_document" "tf_backend" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
+  }
+
+  statement {
+    effect  = "Allow"
+    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    resources = [
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*",
+    ]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["dynamodb:DescribeTable", "dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem"]
+    resources = ["arn:aws:dynamodb:*:*:table/${var.udemy_tf_lock}"]
+  }
+}
+
+resource "aws_iam_policy" "tf_backend" {
+  name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
+  description = "Policy for the CD user to access the Terraform backend"
+  policy      = data.aws_iam_policy_document.tf_backend.json
+}
+
+resource "aws_iam_user_policy_attachment" "tf_backend" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.tf_backend.arn
+}
